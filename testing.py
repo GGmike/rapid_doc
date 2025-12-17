b26e8757167d524eda47af0ad1a2e21b332aeb64
GGmike <miketsun1212@gmail.com>
2025-12-18 00:44:37 +0800

    Update python script to print layout

--- testing.py
+++ testing.py
@@ -2,8 +2,14 @@ import rapid_pdf
 
 items = rapid_pdf.extract_text_from_pdf("test.pdf")
 
-print(items)
+# print(items)
+
+origin_y = items[0].y
 
 for item in items:
-    # print(item.x)
-    print(item.text, item.x, item.y, item.font_size)
+    if item.y != origin_y:
+        print("\n")
+        origin_y = item.y   
+    print(item.text, end=' ')
+
+    # print(item.text, item.x, item.y, item.font_size)
